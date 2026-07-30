08ad593dc8062feda8e6b9b796b745b6ad18bac7
sharrusiddam <sharanyasiddam@gmail.com>
2026-07-30 15:30:31 +0530

    commit

---
+++ sharru.py
@@ -0,0 +1,31 @@
+# a=20
+# print(a)
+# print(type(a))
+
+# a=20.67
+# print(a)
+# print(type(a))
+# a=20+3j
+# print(a)
+# print(type(a))
+# a=None
+# print(a)
+# print(type(a))
+# a=[1,2,3]
+# print(a)
+# print(type(a))
+# a=(4,5,6)
+# print(a)
+# print(type(a))
+# a={4,5,6}
+# print(a)
+# print(type(a))
+# a=20
+# b=30
+# print(a>b)
+
+# for x in range(10):
+#     print(x,end="")
+# a=20
+# b=30
+# print(a>b)
