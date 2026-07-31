396c531049a9df035b09cb664720250ae3fa799e
sharrusiddam <sharanyasiddam@gmail.com>
2026-07-31 16:24:38 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,30 @@
+d1={'AP':'Amaravathi','AP':'Guntur',2:4,8:64,2:16,'Nandyal':'Allagadda'}
+# print(d1)
+# print(type(d1))
+# print(d1.pop(2))
+# print(d1)
+# d1.popitem()
+# print(d1)
+# print(d1.get('AP'))
+# print(d1.keys())
+# print(d1.values())
+# for i in range(1,10):
+#     print(i,end='')
+
+# for i in range(1,10,2):
+#      print('odd numbers are=',i)
+
+# for i in range(0,10,2):
+#      print('even numbers are=',i)
+
+# for i in d1.keys():
+#     print(i)
+
+# for i in d1.items():
+#     print(i)
+
+# d1.setdefault('TN','hyderabad')
+# print(d1)
+
+# d1['AP']='vizac'
+# print(d1)
